8c64f66a582dbb2a19970d4cd740bbfc2a790120
Olivier <oliviervandeneede@hotmail.com>
2021-02-25 20:38:55 +0100

    Example solution

--- src/solution.py
+++ src/solution.py
@@ -20,3 +20,12 @@ class Solution(NamedTuple):
             file.write(f"{len(self.intersections)}\n")
             for intersection in self.intersections:
                 file.write(str(intersection))
+
+
+if __name__ == "__main__":
+    intersection = Intersection(1, [("rue-d-athenes", 2), ("rue-d-amsterdam", 1)])
+    intersection2 = Intersection(0, [("rue-de-londres", 2)])
+    intersection3 = Intersection(2, [("rue-de-moscou", 1)])
+
+    solution = Solution([intersection, intersection2, intersection3])
+    solution.write_output("result.txt")
